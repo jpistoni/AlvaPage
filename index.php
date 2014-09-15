2871656fd87d4d5717ecdf6fcadd92a1723aced3
jpistoni <juanmanuelpistoni@hotmail.com>
2014-09-15 20:07:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<head>
+<link rel="stylesheet" href="index.css" type="text/css" />
+</head>
+
+
+<ul>
+
+  <li><a href="" title=""><span>Home</span></a></li>
+
+  <li><a href="" title="" class="current"><span>About Us</span></a></li>
+
+  <li><a href="" title=""><span>Services</span></a></li>
+
+  <li><a href="" title=""><span>Our Work</span></a></li>
+
+  <li><a href="" title=""><span>Contact Us</span></a></li>
+
+</ul>
+
+</div>
+</div>
